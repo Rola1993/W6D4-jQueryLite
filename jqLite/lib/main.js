2b0e0dd26273aa99663ce49ffa0b2f0630ce3f13
Rola1993 <rola.uiuc@gmail.com>
2018-09-27 17:27:58 -0700

    finish phase 4

--- jqLite/lib/main.js
+++ jqLite/lib/main.js
@@ -22,4 +22,29 @@ window.$l = function(el) {
     }
   });
    
+  function extend(...objects) {
+    
+  }
+  
+  function ajax(options){
+      //step 1 - create xhr object
+    const xhr = new XMLHttpRequest();
+
+    // step 2 - specify path and verb
+    xhr.open(options.type, options.url);
+
+    // step 3 - register a callback
+    xhr.onload = function () {
+      console.log(xhr.status); // for status info
+      console.log(xhr.responseType); //the type of data that was returned
+      console.log(xhr.response); //the actual response. For JSON api calls, this will be a JSON string
+      options.success(xhr.response);
+      
+      if(options.error) options.error();
+    };
+
+    // step 4 - send off the request with optional data
+    xhr.send(options);
+  }
+  
 };
